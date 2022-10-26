084730c22002eac45ddc07694415d64299b12836
jon <jonprosser84@gmail.com>
2022-10-26 16:48:56 +0100

    first commit

---
+++ Grocer.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Grocer {
+    public static void main(String[] args) {
+        
+        /* Task 1: Create an array that stores each aisle:
+             apples – bananas – candy – chocolate – coffee – tea
+
+             Then, use a for loop to print each element in the array. 
+        */
+
+        String[] aisle = {"apples", "banannas", "candy", "chocolate", "coffee", "tea"};
+
+     
+
+        System.out.println("\ndo you sell coffee?");
+        for(int i = 0; i < aisle.length; i++){
+            if(aisle[i] == "coffee"){
+                System.out.println("Yes we have that in aisle " + i);
+                   break;
+             }
+        }
+
+        
+
+    
+ 
+        /* Task 2:
+           1. System.out.println("\nDo you sell coffee?");
+
+           2. Using the for loop from task 1:
+            • check if Java Grocer sells coffee.
+            • if so, break the loop and print: \nWe have that in aisle: <index>
+
+        */
+    
+    }
+}
